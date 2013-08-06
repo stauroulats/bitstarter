b831667e708c63d93b29b1e373195e696ead4b53
stsiamasioti@gmail.com <ubuntu@ip-172-31-22-255.us-west-2.compute.internal>
2013-08-06 20:05:14 +0000

    Edited product desc in index.html

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ var text = buffer.toString();
 response.send(text);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
